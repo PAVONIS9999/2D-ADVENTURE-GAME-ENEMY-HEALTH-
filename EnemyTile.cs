2d25ac291f0067c20ffde5284461e1384d8f2343
PAVONIS9999 <1josephlmg@gmail.com>
2025-09-25 17:08:04 +0200

    Added EnemyTile class

---
+++ EnemyTile.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2D_ADVENTURE_GAME
+{
+    abstract class EnemyTile : CharacterTile
+    {
+        public EnemyTile(Position position ,int HitPoints, int AttackPower) : base(position, HitPoints,AttackPower)
+        {
+        
+        }
+
+        public abstract bool GetMove(out Tile tile); // method to indicate tile enemy will move to
+
+        public abstract CharacterTile[] GetTargets(); 
+
+    }
+}
